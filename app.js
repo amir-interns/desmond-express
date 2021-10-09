9134608159afdfe7a6bfc3209145234e04e594b3
Андрей <grazyjack83@gmail.com>
2021-10-09 08:40:35 +0300

    1

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
 const fs = require(`fs`);
 const express = require(`express`);
 const bodyParser = require(`body-parser`);
-const Web3 = require('web3');
-const web3 = new Web3('wss://mainnet.infura.io/ws/v3/');
 const bitcoin = require('./send.js');
+const Web3 = require('web3');
+const web3 = new Web3('wss://mainnet.infura.io/ws/v3/3f22f5d2f7294954b59850c7c8e08875');
 const app = express();
 const urlencodedParser = bodyParser.urlencoded({extended: false});
+const jsonParser = bodyParser.json();
 let tokenAddress = "0xdac17f958d2ee523a2206206994597c13d831ec7";
 let minABI = [
     // balanceOf
     {
-      "constant":true,
-      "inputs":[{"name":"_owner","type":"address"}],
-      "name":"balanceOf",
-      "outputs":[{"name":"balance","type":"uint256"}],
-      "type":"function"
+        "constant":true,
+        "inputs":[{"name":"_owner","type":"address"}],
+        "name":"balanceOf",
+        "outputs":[{"name":"balance","type":"uint256"}],
+        "type":"function"
     },
     // decimals
     {
-      "constant":true,
-      "inputs":[],
-      "name":"decimals",
-      "outputs":[{"name":"","type":"uint8"}],
-      "type":"function"
+        "constant":true,
+        "inputs":[],
+        "name":"decimals",
+        "outputs":[{"name":"","type":"uint8"}],
+        "type":"function"
     }
-  ];
+];
 
 const contract = new web3.eth.Contract(minABI,tokenAddress);
 let dec;
@@ -37,30 +38,33 @@ app.use('/public', express.static('public'));
 app.get('/', ((req, res) => {
     res.render(`index`);
 }));
+/*
+app.get('/sendBitcoin', jsonParser, (async(req, res) => {
 
-app.get('/sendBitcoin', urlencodedParser, (async(req, res) => {
-    res.render(`btctest`);
-    console.log(await bitcoin.sendBitcoin('mm4fxQbmf97ubVFBMkRNHiNoGTEAuZiGfA', 0.00001));
+    const txResponse = {
+        success: ,
+        transfers:
+    }
+    res.render(`btctest`, data: txResponse);
+    console.log(await bitcoin.sendBitcoin('mm4fxQbmf97ubVFBMkRNHiNoGTEAuZiGfA', 0.0001));
 
-}));
+}));*/
 
-
+//EHT balance-page
 app.post('/checkBalance', urlencodedParser, (async(req, res) => {
-
     const account = {
         address: req.body.address,
         balance: await getBalance(req.body.address),
         usdtBalance: await getUsdtBalance(req.body.address)
     };
-
-    //console.log(10**decimals);
     res.render(`result`, {data: account});
 }));
 
-
-app.get('/about', ((req, res) => {
-    res.send(`Hi!`);
+//BTC balance-page
+app.post('/btctest', jsonParser, (async(req, res) => {
+    res.render(`btctest`, {data: await bitcoin.sendBitcoin(req.body.transfers)});
 }));
+
 //ETH balance
 const getBalance = async(address) => {
     return web3.eth.getBalance(address);
@@ -68,7 +72,7 @@ const getBalance = async(address) => {
 
 //token balance
 const getUsdtBalance = async(walletAddress) => {
-    return (contract.methods.balanceOf(walletAddress).call().then((response) => { return (response / 10**dec)  })); 
+    return (contract.methods.balanceOf(walletAddress).call().then((response) => { return (response / 10**dec)  }));
 };
 
 app.listen(3000);
--- send.js
+++ send.js
@@ -1,66 +1,106 @@
-const sendBitcoin = async (recieverAddress, amountToSend) => {
+const sendBitcoin = async (body) => { //(recieverAddress, amountToSend)
   const axios = require("axios");
   const bitcore = require("bitcore-lib");
-    const sochain_network = "BTCTEST";
-    const privateKey = "92mmY2TTydkHmGpcAWCmqEZjMwxN23TxHgq9HVzSrXxNa17zTWF";
-    const sourceAddress = "n3bduR9Y27yZsfCMFyPJokVhe9KPdd6bEu";
-    const satoshiToSend = amountToSend * 100000000;
-    let fee = 0;
-    let inputCount = 0;
-    let outputCount = 2;
-    const utxos = await axios.get(
-      `https://sochain.com/api/v2/get_tx_unspent/${sochain_network}/${sourceAddress}`
-    );
-    const transaction = new bitcore.Transaction();
-    let totalAmountAvailable = 0;
-  
-    let inputs = [];
-    utxos.data.data.txs.forEach(async (element) => {
-      let utxo = {};
-      utxo.satoshis = Math.floor(Number(element.value) * 100000000);
-      utxo.script = element.script_hex;
-      utxo.address = utxos.data.data.address;
-      utxo.txId = element.txid;
-      utxo.outputIndex = element.output_no;
-      totalAmountAvailable += utxo.satoshis;
-      inputCount += 1;
-      inputs.push(utxo);
-    });
-  
-    transactionSize = inputCount * 146 + outputCount * 34 + 10 - inputCount;
-    // Check if we have enough funds to cover the transaction and the fees assuming we want to pay 20 satoshis per byte
-  
-    fee = transactionSize * 20
-    if (totalAmountAvailable - satoshiToSend - fee  < 0) {
-      throw new Error("Balance is too low for this transaction");
-    }
-  
-    //Set transaction input
-    transaction.from(inputs);
-  console.log(satoshiToSend);
-    // set the recieving address and the amount to send
-    transaction.to(recieverAddress, satoshiToSend);
-  
-    // Set change address - Address to receive the left over funds after transfer
-    transaction.change(sourceAddress);
-  
-    //manually set transaction fees: 20 satoshis per byte
-    transaction.fee(fee * 20);
-  
-    // Sign transaction with your private key
-    transaction.sign(privateKey);
-  
-    // serialize Transactions
-    const serializedTX = transaction.serialize();
-    // Send transaction
-    const result = await axios({
-      method: "POST",
-      url: `https://sochain.com/api/v2/send_tx/${sochain_network}`,
-      data: {
-        tx_hex: serializedTX,
-      },
-    });
-    return result.data.data;
+  const sochain_network = "BTCTEST";
+  const privateKey = "92mmY2TTydkHmGpcAWCmqEZjMwxN23TxHgq9HVzSrXxNa17zTWF";
+  const sourceAddress = "n3bduR9Y27yZsfCMFyPJokVhe9KPdd6bEu";
+  let amountToSend = 0;
+  let mass = [];
+  for (let i of body) {
+    amountToSend = amountToSend + parseFloat(i.value);
   };
+  const satoshiToSend = amountToSend * 100000000; //*
+  let fee = 0;
+  let inputCount = 0;
+  let outputCount = 1;
+  for (let i of body) {
+    outputCount++;
+  };
+  if (outputCount > 50) {
+    throw new Error("Too much transactions. Max 50.");
+  }
+  const utxos = await axios.get(
+      `https://sochain.com/api/v2/get_tx_unspent/${sochain_network}/${sourceAddress}`
+  );
+  const transaction = new bitcore.Transaction();
+  let totalAmountAvailable = 0;
 
-  module.exports.sendBitcoin = sendBitcoin;
+  let inputs = [];
+  utxos.data.data.txs.forEach(async (element) => {
+    let utxo = {};
+    utxo.satoshis = Math.floor(Number(element.value) * 100000000);
+    utxo.script = element.script_hex;
+    utxo.address = utxos.data.data.address;
+    utxo.txId = element.txid;
+    utxo.outputIndex = element.output_no;
+    totalAmountAvailable += utxo.satoshis;
+    inputCount += 1;
+    inputs.push(utxo);
+  });
+
+  transactionSize = inputCount * 146 + outputCount * 34 + 10 - inputCount;
+  // Check if we have enough funds to cover the transaction and the fees assuming we want to pay 20 satoshis per byte
+
+  fee = transactionSize * 1;
+  if (totalAmountAvailable - satoshiToSend - fee  < 0) {
+    throw new Error("Balance is too low for this transaction");
+  }
+
+  //Set transaction input
+  transaction.from(inputs);
+
+  // set the recieving address and the amount to send
+  for (let i of body) {
+
+    if (totalAmountAvailable - transaction.outputAmount - fee - (parseFloat(i.value)*100000000)  < 0) {
+      break;
+    }
+    transaction.to(i.to, parseFloat(i.value)*100000000);
+
+    mass.push({
+      to: i.to,
+      value: i.value,
+      transactionHash: ""
+    });
+    //mass2.push(i.value);
+  }
+
+  //transaction.to(recieverAddress, satoshiToSend);
+
+  // Set change address - Address to receive the left over funds after transfer
+  transaction.change(sourceAddress);
+
+  //manually set transaction fees: 20 satoshis per byte
+  transaction.fee(fee * 20);
+
+  // Sign transaction with your private key
+  transaction.sign(privateKey);
+
+  // serialize Transactions
+  const serializedTX = transaction.serialize();
+  // Send transaction
+  const result = await axios({
+    method: "POST",
+    url: `https://sochain.com/api/v2/send_tx/${sochain_network}`,
+    data: {
+      tx_hex: serializedTX,
+    },
+  });
+  mass.forEach( element => {
+        element.transactionHash =  serializedTX;
+      }
+   );
+  const displayTxData = await axios({
+    method: "GET",
+    url: `https://sochain.com/api/v2/tx/${sochain_network}/${await result.data.data.txid}`
+  });
+  console.log(displayTxData.data.data.outputs);
+
+  const responseData = {
+    status: result.data.status,
+    transfers: mass
+  };
+  return responseData;
+};
+
+module.exports.sendBitcoin = sendBitcoin;
